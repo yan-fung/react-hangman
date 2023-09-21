331b6d1292e87bbf2b9ff074ecb1b32d56f67167
Yan Fung <fungmingyan@hotmail.com>
2023-09-21 23:23:44 +0100

    Refine keyboard rendering with styling

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,10 @@ const App = () => {
       <div style={{ fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+
     </div>
   )
 }
