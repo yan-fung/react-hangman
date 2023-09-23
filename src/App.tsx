95d64e486b240cc261f46771eda2869ea7791282
Yan Fung <fungmingyan@hotmail.com>
2023-09-24 00:38:51 +0100

    Added keyboard input for guessed letters and improved button styling for active and inactive letters

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,10 @@ const App = () => {
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>
-        <Keyboard />
+        <Keyboard activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))}
+        inactiveLetters={incorrectLetters}
+        addGuessedLetter={addGuessedLetter}
+        />
       </div>
 
     </div>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -2,14 +2,27 @@ import styles from "../Keyboard.module.css";
 
 const KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-const Keyboard = () => {
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+}
+
+const Keyboard = ({ activeLetters, inactiveLetters, addGuessedLetter }: KeyboardProps) => {
   return <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem" }}>
     {KEYS.map(key => {
+      const isActive = activeLetters.includes(key);
+      const isInactive = inactiveLetters.includes(key);
         return (
-            <button className={`${styles.btn}`}>{key}</button>
+            <button className={`${styles.btn} ${isActive ? styles.active: ""} ${isInactive ? styles.inactive: ""} `} 
+              disabled={isActive || isInactive}
+              key={key} 
+              onClick={() => addGuessedLetter(key)}>
+              {key}
+            </button>
         )
     })}
   </div>
 }
 
-export default Keyboard;
+export default Keyboard; 
