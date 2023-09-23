982d721e9f5ad80b0681de2efb329a2f98d65ce8
Yan Fung <fungmingyan@hotmail.com>
2023-09-23 22:32:20 +0100

    Created BODY_PARTS array and implemented rendering logic based on numberOfGuesses

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -22,15 +22,16 @@ const LEFT_LEG = (
 <div style={{ width:"100px", height: "10px", background: "black", position: "absolute" , top: "210px", right: "10px", rotate: "-50deg", transformOrigin: "top right" }} />
 )
 
-const HangmanDrawing = () => {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number
+}
+
+const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
   return (
     <div style={{ position: "relative" }}>
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        {RIGHT_LEG}
-        {LEFT_LEG}
+        {BODY_PARTS.slice(0, numberOfGuesses)}
         <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", right: "0", top: "0" }} />
         <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px"}} />
         <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px"}} />
