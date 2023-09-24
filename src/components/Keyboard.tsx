f304b482de8997f6d570fabf3e15813afd9d0fd5
Yan Fung <fungmingyan@hotmail.com>
2023-09-24 10:44:14 +0100

    Improved button rendering with different styles for active, inactive, and disabled states

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -6,16 +6,17 @@ type KeyboardProps = {
   activeLetters: string[];
   inactiveLetters: string[];
   addGuessedLetter: (letter: string) => void;
+  disabled?: boolean;
 }
 
-const Keyboard = ({ activeLetters, inactiveLetters, addGuessedLetter }: KeyboardProps) => {
+const Keyboard = ({ activeLetters, inactiveLetters, addGuessedLetter, disabled = false }: KeyboardProps) => {
   return <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem" }}>
     {KEYS.map(key => {
       const isActive = activeLetters.includes(key);
       const isInactive = inactiveLetters.includes(key);
         return (
             <button className={`${styles.btn} ${isActive ? styles.active: ""} ${isInactive ? styles.inactive: ""} `} 
-              disabled={isActive || isInactive}
+              disabled={isActive || isInactive || disabled}
               key={key} 
               onClick={() => addGuessedLetter(key)}>
               {key}
