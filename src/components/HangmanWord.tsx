fc8c81c03ca415bdcd1dcf944a2c6c4980366ace
Yan Fung <fungmingyan@hotmail.com>
2023-09-24 10:46:52 +0100

    Adjusted word colouring based on the reveal state

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,15 +1,17 @@
 type HangmanWordProps = {
-  guessedLetters: string[]
-  wordToGuess: string
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
 }
 
-const HangmanWord = ({ guessedLetters, wordToGuess }: HangmanWordProps) => {
+const HangmanWord = ({ guessedLetters, wordToGuess, reveal = false }: HangmanWordProps) => {
   return (
     <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
         {wordToGuess.split("").map((letter, index) => (
             <span style={{ borderBottom: ".1rem solid black" }} key={index}>
                 <span style={{
-                    visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                    visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+                    color: !guessedLetters.includes(letter) ? "red" : "black"
 
                 }}>
                 {letter}
